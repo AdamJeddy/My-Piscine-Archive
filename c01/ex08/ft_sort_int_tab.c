e1c985044e2a6607368df3700ad3e5b0a53f4c21
AdamJeddy <adamjeddy@outlook.com>
2021-09-14 16:24:12 +0400

    c01

---
+++ c01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aahsan <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/08 10:59:19 by aahsan            #+#    #+#             */
+/*   Updated: 2021/08/08 11:43:08 by aahsan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int		swap;
+	int		count;
+
+	count = 0;
+	while (count < (size - 1))
+	{
+		if (tab[count] > tab[count + 1])
+		{
+			swap = tab[count];
+			tab[count] = tab[count + 1];
+			tab[count + 1] = swap;
+			count = 0;
+		}
+		else
+			count++;
+	}
+}
