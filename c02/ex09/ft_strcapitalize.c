f29992758055fa1a4e95bea0d481b8ff30b87a66
AdamJeddy <adamjeddy@outlook.com>
2021-09-14 18:53:41 +0400

    c02

---
+++ c02/ex09/ft_strcapitalize.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aahsan <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/09 12:19:30 by aahsan            #+#    #+#             */
+/*   Updated: 2021/08/10 11:11:41 by aahsan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		check_letter(char a)
+{
+	if ((a <= 'z' && a >= 'a') || ((a <= 'Z' && a >= 'A')))
+		return (1);
+	return (0);
+}
+
+int		check_alpha(char a)
+{
+	if ((a >= 'a' && a <= 'z')
+			|| (a >= 'A' && a <= 'Z') || (a >= '0' && a <= '9'))
+		return (1);
+	return (0);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int i;
+
+	i = -1;
+	while (str[++i] != '\0')
+	{
+		if (check_letter(str[i]) && !check_alpha(str[i - 1]))
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+		}
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] += 32;
+		}
+	}
+	return (str);
+}
---
+++ c02/ex11/ft_putstr_non_printable.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aahsan <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/10 12:41:52 by aahsan            #+#    #+#             */
+/*   Updated: 2021/08/10 14:24:43 by aahsan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	print_hex(char c)
+{
+	write(1, "\\", 1);
+	write(1, &"0123456789abcdef"[c / 16], 1);
+	write(1, &"0123456789abcdef"[c % 16], 1);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int	i;
+
+	i = -1;
+	while (str[++i])
+	{
+		if (str[i] < 32 || str[i] > 126)
+			print_hex(str[i]);
+		else
+			write(1, &str[i], 1);
+	}
+}
