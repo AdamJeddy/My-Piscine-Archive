61bab53d6706d404261fe350bc3e37d7b4286d33
Jeddy <58102753+AdamJeddy@users.noreply.github.com>
2021-12-25 16:57:53 +0400

    Update main.c

--- c03/main.c
+++ c03/main.c
@@ -16,10 +16,11 @@ int main(int argc, char **argv)
 	char *s3 = argv[3];
 
 	// ex00 - ft_strcmp
-// 	printf("my strcmp: %d\nstrcmp: %d\n", ft_strcmp(s1, s2), strcmp(s1, s2));
+ 	printf("my strcmp: %d\nstrcmp: %d\n", ft_strcmp(s1, s2), strcmp(s1, s2));
 	
 	// ex01 - ft_strncmp
-/*	printf("my strcmp: %d\tstrcmp: %d\n", ft_strncmp("", "", 0), strncmp("", "", 0));
+///*	
+	printf("my strcmp: %d\tstrcmp: %d\n", ft_strncmp("", "", 0), strncmp("", "", 0));
 	printf("my strcmp: %d\tstrcmp: %d\n", ft_strncmp("", "", 2), strncmp("", "", 2));
 	printf("my strcmp: %d\tstrcmp: %d\n", ft_strncmp("", "", -1), strncmp("", "", -1));
 	printf("my strcmp: %d\tstrcmp: %d\n", ft_strncmp("nice", "nice", 2), strncmp("nice", "nice", 2));
@@ -29,19 +30,21 @@ int main(int argc, char **argv)
 	printf("my strcmp: %d\tstrcmp: %d\n", ft_strncmp("", "asd", 2), strncmp("", "asd", 2));
 	printf("my strcmp: %d\tstrcmp: %d\n", ft_strncmp("asd", "", 1), strncmp("asd", "", 1));
 	printf("my strcmp: %d\tstrcmp: %d\n", ft_strncmp("asd4", "asd7", 4), strncmp("asd4", "asd7", 4));
-*/
+//*/
 	
 	// ex02 - ft_strcat
-/* 	ft_strcat(s1, s2);
+///* 	
+	ft_strcat(s1, s2);
 	printf("%s", s1);
-*/
+//*/
 
 	// ex03 - ft_strncat
-/*	ft_strncat(s1, s2, atoi(argv[3]));
+///*	
+	ft_strncat(s1, s2, atoi(argv[3]));
 	printf("%s", s1);
-*/
+//*/
 
 	// ex04 - ft_strstr
-// 	printf("%s", ft_strstr(s1, s2));
+ 	printf("%s", ft_strstr(s1, s2));
 
 }
