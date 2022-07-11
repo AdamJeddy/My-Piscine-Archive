14aaabdabba6571a46543bc288aca34050a7c774
Adam <adamjeddy@outlook.com>
2022-07-11 16:23:23 +0400

    c06

---
+++ c06/ex00/ft_print_program_name.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aahsan				                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/11 22:13:49 by aahsan            #+#    #+#             */
+/*   Updated: 2021/08/11 22:24:38 by aahsan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		ft_strlen(char *str)
+{
+	int count;
+
+	count = 0;
+	while (*str++)
+		count++;
+	return (count);
+}
+
+int		main(int argc, char **argv)
+{
+	(void)argc;
+	write(1, argv[0], ft_strlen(argv[0]));
+	write(1, "\n", 1);
+	return (0);
+}
